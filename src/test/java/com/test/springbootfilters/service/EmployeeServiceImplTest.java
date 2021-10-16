02e475d76f449800b6581f1f46a23f6da171b911
AjithKumar <mailajithkumars@gmail.com>
2021-10-16 20:50:03 +0530

    Added Junit Test class for EmployeeService Layer

---
+++ src/test/java/com/test/springbootfilters/service/EmployeeServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.test.springbootfilters.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeServiceImplTest {
+
+    @Test
+    void getAllEmployees() {
+    }
+
+    @Test
+    void getEmployeesByFilters() {
+    }
+
+    @Test
+    void addEmployee() {
+    }
+}
